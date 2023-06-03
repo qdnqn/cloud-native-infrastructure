7107bfcb6da497e4e9414ded405e8e8178dbadb5
qdnqn <adnn.selimovic@gmail.com>
2023-06-03 14:33:40 +0200

    Optionalize install on startup and add argocd install

--- scripts/setup-infrastructure.sh
+++ scripts/setup-infrastructure.sh
@@ -12,13 +12,23 @@ if [[ ! -f "/home/qdnqn/.run.once" ]]; then
 
   docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
-  kubectl create ns kafka
-  kubectl create ns kafdrop
+  for arg in "$@"
+  do
+      if [[ $arg == "kafka"]]; then
+          kubectl create ns kafka
+          helm upgrade --install kafka ../charts/kafka --namespace kafka --values ../charts/kafka/values.yaml
+      elif [[ $arg == "kafdrop" ]]; then
+          kubectl create ns kafdrop
+          helm upgrade --install kafka ../charts/kafdrop --namespace kafdrop --values ../charts/kafdrop/values.yaml
+      elif [[ $arg == "kafdrop" ]]; then
+          kubectl create ns argocd
+          helm upgrade --install argocd ../charts/argocd --namespace argocd --values ../charts/argocd/values.yaml
+      else
+        echo "Invalid argument $arg"
+      fi
+  done
 
   kubectl apply -f resources/raw/yaml/setup/traefik-config-k3s.yaml
-
-  helm upgrade --install kafka ../charts/kafka --namespace kafka --values ../charts/kafka/values.yaml
-  helm upgrade --install kafka ../charts/kafdrop --namespace kafdrop --values ../charts/kafdrop/values.yaml
 
   VM_IP=$(hostname -I | cut -d " " -f1)
   sed -i "s/{VM_IP}/${VM_IP}/g" resources/raw/yaml/setup/ingresses.yaml
